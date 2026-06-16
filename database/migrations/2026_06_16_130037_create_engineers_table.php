fb8906d8bd4d07ae02713cdbbfc3c6e6014c7238
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-06-16 17:14:44 +0300

    added user table

---
+++ database/migrations/2026_06_16_130037_create_engineers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('engineers', function (Blueprint $table) {
+      $table->id();
+      $table->foreignIdFor(User::class)->constrained();
+      $table->string('job_title');
+      $table->decimal('base_salary', 15, 2);
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('engineers');
+  }
+};
