8845e9acad94913f06b0ad6c5d12d3020821eb43
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-07-27 14:39:03 +0300

    edit in revenu

--- app/Http/Resources/RevenueResource.php
+++ app/Http/Resources/RevenueResource.php
@@ -14,10 +14,38 @@ class RevenueResource extends JsonResource
    */
   public function toArray(Request $request): array
   {
+    $revenueableInfo = null;
+
+    if ($this->relationLoaded('revenueable') && $this->revenueable) {
+      $type = $this->revenueable_type;
+
+      if ($type === 'App\Models\CurrencyFund') {
+        $revenueableInfo = [
+          'type'    => 'user',
+          'user_id' => $this->revenueable->fund?->user_id ?? null,
+          'fund_id' => $this->revenueable->fund_id ?? null,
+          'id'      => $this->revenueable->id,
+        ];
+      } elseif ($type === 'App\Models\CompanyFundCurrency') {
+        $revenueableInfo = [
+          'type'            => 'company',
+          'company_fund_id' => $this->revenueable->company_fund_id ?? null,
+          'id'              => $this->revenueable->id,
+        ];
+      } elseif ($type === 'App\Models\ProjectFundCurrency') {
+        $revenueableInfo = [
+          'type'            => 'project',
+          'project_fund_id' => $this->revenueable->project_fund_id ?? null,
+          'id'              => $this->revenueable->id,
+        ];
+      }
+    }
+
     return [
       'id'                => $this->id,
       'revenueable_type'  => $this->revenueable_type,
       'revenueable_id'    => $this->revenueable_id,
+      'revenueable_info'  => $revenueableInfo,
       'statement'         => $this->statement,
       'amount'            => (float) $this->amount,
       'is_posted'         => (bool) $this->is_posted,
